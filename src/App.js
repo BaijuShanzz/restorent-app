37fb7a7de7933d9322c4d9a988ec03d3cd0c9b5f
Baiju shanzz <baijushanzz@gmail.com>
2023-07-09 15:00:21 +0530

    dishes page completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Welcome from "./Components/Pages/Welcome/Welcome";
 import Footer from "./Components/Footer/Footer";
 import Partner from "./Components/Pages/Partner/Partner";
 import Booking from "./Components/Pages/Booking/Booking";
+import Dishes from "./Components/Pages/Dishes/Dishes";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Welcome/>
       <Partner/>
       <Booking />
+      <Dishes />
       <Footer />
     </>
   );
